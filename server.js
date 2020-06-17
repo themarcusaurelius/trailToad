e4dd315af223d3d63f800f3e762332c6ebcaaf53
unknown <webdevmark16@gmail.com>
2020-06-16 21:28:30 -0500

    Comitting

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const store = require('./routes/api/store');
   
 const app = express(); 
-   
+  
 // Init Middleware
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: true }));
@@ -20,7 +20,7 @@ app.use('/api/data', product);
 //Configuration for Express to behave correctly in production environment
 if (process.env.NODE_ENV === 'production') {
     //First - Making sure express will serve production assets - main.js, main.css, etc
-    app.use(express.static(__dirname, 'build'));
+    app.use(express.static('client/build'));
     //Second -Express will serve up the index.html file if it doesn't recognize the route
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
